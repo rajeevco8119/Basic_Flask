158109d8e8e7868de2d01931cfb291456bdca21c
rajeevco8119 <rajeev.co8119@gmail.com>
2020-04-11 01:32:10 +0530

    Create flask_mongo.py
    This file uses flask_pymongo library to perform the CRUD Operations 
    Here the database used is MongoDB and Database name is Star
    star is having 2 attributes - name and distance
    You can check MongoDB compass software to check for outputs in mongodb
    https://www.mongodb.com/products/compass

---
+++ flask_mongo.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+#Example dbname in python
+app.config['MONGO_DBNAME'] = 'restdb'
+
+#Example port for MongoDB port setup
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+mongo = PyMongo(app)
+
+#READ ALL ENTRIES
+@app.route('/stars',methods=['GET'])
+def get_all_frameworks():
+    star = mongo.db.stars
+    output = []
+    for q in star.find():
+        output.append({'name': q['name'],'distance': q['distance']})
+    return jsonify({'result':output})
+
+# READ ID wise
+@app.route('/stars/<name>',methods=['GET'])
+def get_single_frameworks(name):
+    output = []
+    star = mongo.db.stars
+    q = star.find_one({'name': name})
+    if q:
+        output.append({'name': q['name'],'distance': q['distance']})
+    else:
+        output =[]
+    return jsonify({'result':output})
+
+#CREATE
+@app.route('/stars',methods=['POST'])
+def add_single_framework():
+    star = mongo.db.stars
+    name = request.json['name']
+    distance = request.json['distance']
+
+    #Function will assign star_id
+    star_id = star.insert({'name':name,'distance':distance})
+    new_star = star.find_one({'_id': star_id})
+    output = {'name':new_star['name'],'distance':new_star['distance']}
+    return jsonify({'result': output})
+
+#UPDATE
+@app.route('/stars/<name>',methods=['PUT'])
+def update_single_framework(name):
+    star = mongo.db.stars
+    new_star = star.find_one({'name': name})
+    new_star['name'] = request.json['name']
+    new_star['distance'] = request.json['distance']
+    star.save(new_star)
+    return jsonify({'result': 'Update successful'})
+
+#DELETE
+@app.route('/stars/<name>',methods=['DELETE'])
+def delete_single_framework(name):
+    star = mongo.db.stars
+    new_star = star.find_one({'name': name})
+    star.delete_one(new_star)
+    return jsonify({'result': 'Delete successful'})
+
+
+if __name__=='__main__':
+    app.run(debug=True)
